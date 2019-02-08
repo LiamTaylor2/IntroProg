91aff687484b39da1a95bf53bdbc36de2ab47870
LiamTaylor2 <47446843+LiamTaylor2@users.noreply.github.com>
2019-02-08 12:02:54 +0200

    Add files via upload

---
+++ chapter08.js
@@ -0,0 +1,16 @@
+var dreamCar = {
+    make: "Oldsmobile",
+    model: "98",
+    color: "brown",
+    year: 1983,
+    bodyStyle: "Luxury Car",
+    price: 4500
+};
+
+document.getElementById("pricetag").innerHTML = dreamCar.price;
+document.getElementById("modelyear").innerHTML = dreamCar.year;
+document.getElementById("body").style.backgroundColor = dreamCar.color;
+document.getElementById("body").innerHTML = dreamCar.make + " " + dreamCar.model;
+document.getElementById("body").style.height = 200;
+document.getElementById("backwheel").style.top = 230;
+document.getElementById("frontwheel").style.top = 230;
